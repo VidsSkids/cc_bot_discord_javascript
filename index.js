011cadc47b936c7445a772a07fb8040b0bd9ba9d
Victor Massonnat <vic.massonnatdu38@gmail.com>
2020-11-21 15:13:33 +0100

    first commit

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const bot = new Discord.Client();
 bot.on("ready", function () {
     console.log("Carapuce est dans les places !");
 });
-bot.login("MON_TOKEN");
+
+bot.on("message", message => {
+    if (message.content == "!ping") {
+        message.channel.send("Carapong !");
+    }
+    if (message.content == "!carabonjour") {
+        message.reply("Carabonjour a toi !")
+        message.react("😃") 
+    }
+})
+
+bot.login("Nzc5Njk3MDU1Nzc3MDMwMTQ0.X7kTeA.cU7-UXU20qlKsjYzhAcaqRAWx1w");
